afafd8a5da25f6e7e8326b8c2f06d6ca8eb08e4c
Ainun Nadhifah Syamsiyah <ainunsyamsiyah@gmail.com>
2024-09-26 07:22:51 +0700

    feat: add create items page

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ Route::get('/items/{id}/show', [ItemController::class, 'show'])->name('items.sho
 Route::delete('/items/{id}/destroy', [ItemController::class, 'destroy'])->name('items.destroy');
 Route::get('/items/{id}/edit', [ItemController::class, 'edit'])->name('items.edit');
 Route::post('/items/{id}/update', [ItemController::class, 'update'])->name('items.update');
-Route::post('items/{id}/buy', [ItemController::class, 'buy'])->name('items.buy');
+Route::post('/items/{id}/buy', [ItemController::class, 'buy'])->name('items.buy');
